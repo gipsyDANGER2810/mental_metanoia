de7260cf8076cb410280634066db570828a9237b
gipsyDANGER2810 <swapnilgalhate1036@gmail.com>
2024-02-16 16:01:23 +0000

    Login component added

---
+++ src/components/login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">First Name</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Last Name</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Sex</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Date of Birth</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Password</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+        </div>
+    )
+}
+
+export default Login
